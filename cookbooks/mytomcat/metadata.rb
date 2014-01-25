19a03805d55591c5af5fd02a6a076a4e1f8a58dd
ktang <khiem_tang@intuit.com>
2014-01-24 23:43:21 -0800

    commited to wrong location

--- cookbooks/mytomcat/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'mytomcat'
-maintainer       'Intuit, Inc.'
-maintainer_email 'khiem_tang@intuit.com'
-license          'Apache 2.0'
-description      'Installs/Configures mytomcat'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
